bb3eac38cda8e704fed340c268ebc96a3cf2e886
EmilyR12213 <erob@brstatus.com>
2026-09-14 16:10:01 +0200

    Add plot 4

---
+++ plot4.R
@@ -0,0 +1,16 @@
+
+#code to create plot 4
+> png("plot4.png", width = 480, height = 480)
+> par(mfrow = c(2, 2))
+> plot(data$DateTime, data$Global_active_power, type = "l", ylab = "Global Active Power", xlab = "", xaxt = "n")
+> axis(1, at = as.POSIXct(c("2007-02-01", "2007-02-02", "2007-02-03")), labels = c("Thu", "Fri", "Sat"))
+> plot(data$DateTime, data$Voltage, type = "l", ylab = "Voltage", xlab = "datetime", xaxt = "n")
+> axis(1, at = as.POSIXct(c("2007-02-01", "2007-02-02", "2007-02-03")), labels = c("Thu", "Fri", "Sat"))
+> plot(data$DateTime, data$Sub_metering_1, type = "l", col = "black", ylab = "Energy sub metering", xlab = "", xaxt = "n")
+> lines(data$DateTime, data$Sub_metering_2, col = "red")
+> lines(data$DateTime, data$Sub_metering_3, col = "blue")
+> axis(1, at = as.POSIXct(c("2007-02-01", "2007-02-02", "2007-02-03")), labels = c("Thu", "Fri", "Sat"))
+> legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col = c("black", "red", "blue"), lty = 1, bty = "n")
+> plot(data$DateTime, data$Global_reactive_power, type = "l", ylab = "Global_reactive_power", xlab = "datetime", xaxt = "n")
+> axis(1, at = as.POSIXct(c("2007-02-01", "2007-02-02", "2007-02-03")), labels = c("Thu", "Fri", "Sat"))
+> dev.off()
